5c8d9783f7d84252805f510282065ff62ea52638
dlf57 <dfolmsbee@gmail.com>
2017-06-14 10:41:23 -0400

    Running ML w/ database
    Trying to get the database to run with the ML but having issues trying to convert the data into appropriate datatype for the ML.

--- ML.py
+++ ML.py
@@ -1,55 +1,83 @@
-import pickle
+import sqlite3
 import numpy as np
-import modified_molml
-#import AtomizationE
 import matplotlib.pyplot as plt
-import itertools
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.feature_extraction import DictVectorizer
 
-pickle_in = open('Molecule_Dataset.pickle', 'rb')
-molecules = []
-while 1:
-    try:
-        molecules.append(pickle.load(pickle_in))
-    except EOFError:
-        break
-#print molecules
+conn = sqlite3.connect('MolecularData.db')
+cursor = conn.cursor()
 
-#pickle_in = open('Molecule_Data.pickle', 'rb')
-#molecule_data = pickle.load(pickle_in)
-#print molecule_data
+# Need to read through the database
+cursor.execute('SELECT Name, Bonds, Angles, Torsions, Energy FROM MoleculeData')
+for row in cursor:
+    name = row[0]
+    bonds = row[1]
+    angles = row[2]
+    torsions = row[3]
+    energy = row[4]
 
-#A = modified_molml.molecules
-A = molecules
-#B = AtomizationE.mol.energy
+    # Bonds, Angles, and Torsions are converted into unicode and must be converted to string
+    bond = unicode.encode(bonds)
+    angle = unicode.encode(angles)
+    torsion = unicode.encode(torsions)
 
-print A
+    Bond = bond.split(";")
+    Angle = angle.split(";")
+    Torsion = torsion.split(";")
 
-# Training input values from given data set
-#X = A
+    # Bonds = float(Bond)
+    # Angles = float(Angle)
+    # Torsions = float(Torsion)
 
-# Training target values atomization energy
-#y = B
+    # Bond = np.asarray(bond)
+    # Angle = np.asarray(angle)
+    # Torsion = np.asarray(torsion)
+    # molecule = np.asarray([Bond, Angle, Torsion])
 
-# Test inputs ideally from other conformers
-#X_test = Will uncomment if we choose to test against conformers
-
-# Might not do this if we use the confomers as a test
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-#reg = KernelRidge()
-# Training of the model
-#reg.fit(X_train, Y_train)
-
-# Check to see accuracy of the training
-#accuracy = reg.score(X_train, Y_train)
-#print(accuracy)
-
-# Predicting the output of the conformers
-#predict = clf.predict(X_test, Y_test)
-#print(predict)
+    # molecule = bond, angle,torsion
+    # print type(Bond)
 
 
+    # vec = DictVectorizer()
+    # vec.fit_transform(bond).toarray()
+    # print vec
+
+
+    # X = [molecule]
+    # X = preprocessing.scale(X)
+    # y = [energy]
+    #
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    #
+    # reg =KernelRidge()
+    # reg.fit(X_train, y_train)
+    #
+    # accuracy = reg.score(X_train, y_train)
+    # print accuracy
+    #
+    # predict = reg.predict(X_test, y_test)
+    # print(predict)
+
+cursor.close()
+conn.close()
+
+
+# # Test inputs ideally from other conformers
+# #X_test = Will uncomment if we choose to test against conformers
+#
+# # Might not do this if we use the confomers as a test
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#
+# reg = KernelRidge()
+# # Training of the model
+# reg.fit(X_train, y_train)
+#
+# # Check to see accuracy of the training
+# accuracy = reg.score(X_train, y_train)
+# print(accuracy)
+#
+# # Predicting the output of the conformers
+# #predict = clf.predict(X_test, Y_test)
+# #print(predict)
