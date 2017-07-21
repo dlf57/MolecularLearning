628f8f0076c130c4bb887bf911c93fe88559fc41
dlf57 <dfolmsbee@gmail.com>
2017-07-21 15:59:21 -0400

    Cleanup
    I tried to clean up the code and make comments explaining what is going on in certain parts.

--- molML.py
+++ molML.py
@@ -37,13 +37,10 @@ for argument in sys.argv[1:]:
     except:
         pass
 
-
     # In this case I do not include Energy as that is our dependent variable
     #print mol.energy # in kcal/mol
     # ideally, we should turn this into an atomization energy\
     energy = [mol.energy]
-    # energy = np.array(mol.energy)
-
 
     # iterate through all atoms
     #  .. this is commented out because Bag Of Bonds doesn't use atomic charges
@@ -62,11 +59,9 @@ for argument in sys.argv[1:]:
         # bonds.append("%s-%s, %8.4f" % (begin, end, bond.GetLength()) )
         bonds.append("%8.4f" % (bond.GetLength()))
         # print (bonds[-1])
-        # np.array(bonds)
     bonds = bonds + ([None] * 98)
     bonds = np.asarray(bonds, dtype=float)
     bonds[np.isnan(bonds)] = -99999
-
 
     # iterate through all angles
     angles = []
@@ -87,7 +82,6 @@ for argument in sys.argv[1:]:
     angles = angles + ([None] * 53)
     angles = np.asarray(angles, dtype=float)
     angles[np.isnan(angles)] = -99999
-
 
     # iterate through all torsions
     torsions = []
@@ -114,52 +108,51 @@ for argument in sys.argv[1:]:
             #print (torsions[-1])
     torsions = np.asarray(torsions, dtype=float)
 
+    # store the bonds angles and torsions as molecule descriptors
+    molecule_descriptor = np.concatenate((bonds,angles,torsions))
 
-    mol = np.concatenate((bonds,angles,torsions))
-
+    # store the descriptors for the molecules in a dictionary
     dict1 = {}
-    dict1.update({'Molecule': mol})
+    dict1.update({'Molecule': molecule_descriptor})
     dict1.update({'Name': name})
-    dict1.update({'Bonds': bonds})
-    dict1.update({'Angles': angles})
-    dict1.update({'Torsions': torsions})
+    # dict1.update({'Bonds': bonds})
+    # dict1.update({'Angles': angles})
+    # dict1.update({'Torsions': torsions})
     dict1.update({'Energy': energy})
     row_list.append(dict1)
 
+# make a dataframe of all the molecules, their descriptors and energies
 # df = pd.DataFrame(row_list, columns=['Name','Bonds','Angles','Torsions', 'Energy'])
 df = pd.DataFrame(row_list, columns=['Name', 'Molecule', 'Energy'])
-# molecules = df['Bonds']
-# molecules = np.column_stack((df['Bonds'],df['Angles'],df['Torsions']))
 molecules = df['Molecule']
 Energy = df['Energy']
 
 X = np.asarray(list(molecules), dtype=np.float)
 y = np.asarray(list(Energy), dtype=np.float)
 
-print (X)
-print (X.dtype)
-
+# split the molecules up into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-print (X_train.shape)
-print (y_train.shape)
+# print (X_train.shape)
+# print (y_train.shape)
 
 clf = KernelRidge()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test,y_test)
 print(accuracy)
 
-
 predicted = clf.predict(X_test)
-print (predicted)
-print (y_test)
 
-df1 = pd.DataFrame(predicted, columns=['Predicted'])
-df2 = pd.DataFrame(y_test, columns=['Actual'])
+# dataframe to organize the predicted and actual values (just visual for me, feel free to comment out)
+df_predicted = pd.DataFrame(predicted, columns=['Predicted'])
+df_actual = pd.DataFrame(y_test, columns=['Actual'])
+print (df_predicted)
+print (df_actual)
 
-
+# plotting the actual vs. the predicted to get a visual representation
+plt.figure(figsize=(8,6))
 plt.scatter(y_test, predicted)
-# plt.legend(loc=4)
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
+plt.title('Comparison of Energies', fontsize=16)
+plt.xlabel('Actual', fontsize=12)
+plt.ylabel('Predicted', fontsize=12)
+plt.grid(True)
 plt.show()
