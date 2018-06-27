4f2266a0e7a5bd220784e380a399a79645ed027e
Dakota Folmsbee <dfolmsbee@gmail.com>
2018-06-27 18:44:50 -0400

    Updated encode method now faster

--- molecular_learning/mol_ml.py
+++ molecular_learning/mol_ml.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+import pickle
 import glob
 import numpy as np
 import pybel
@@ -8,33 +10,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.svm import SVR
 from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
-import matplotlib.pyplot as plt
-from matplotlib import style
+from itertools import repeat
 
 # don't need to reimport openbabel
 ob = pybel.ob
 
-# syntax:
-# molml.py
+# Organization of all paths for the code
+jobs_directory = '/home/dakota/Molecules/QM9/QM9-sdf/'  # molecule directory
+jobs_file = '*.sdf'  # molecule files
+jobs_file_format = 'sdf'  # molecule filetype
 
-# organization of all paths for the code
-csv_file = '/Users/dakota/Documents/Research/data-all_ordered.csv'
-csv_file_columns = ['Name', 'Conformer', 'dftE', 'pm7E', 'mmffE']
-jobs_directory = "/Users/dakota/Documents/Research/conformers/*jobs/*"
-jobs_file = "/rmsd*.mol"
-jobs_file_format = "mol"
-figure_path = '/Users/dakota/Documents/Research/data/Figures/'
-figure_name = 'figure_name.png'
-save_figure = figure_path + figure_name
+# import csv with properties
+molec_csv = '/home/dakota/Molecules/QM9/qm9-rev2.csv'
+csv_all = pd.read_csv(molec_csv)
 
-# Import the energy values
-df_csv = pd.read_csv(csv_file)
-df_csv.columns = csv_file_columns
-df_csv = df_csv.replace('nan', np.nan, regex=True)
-# If there is not a dftE than it is dropped so that
-# . there are no errors of nan when training
-df_csv = df_csv.dropna(axis=0, how='any')
+# to get name split directory
+start_split = jobs_directory.count('/')
+end_split = start_split + 1
 
 
 def atomType(mol, atomIdx):
@@ -42,240 +39,186 @@ def atomType(mol, atomIdx):
     return mol.OBMol.GetAtom(atomIdx).GetType()
 
 
-def encoding(atom):
-    '''Atom encoding
-    Each atom encoding is represented by a string of numbers
-    consisting of an element encoding and a hybrid encoding.
+encode = {'Br': '350', 'C+': '064', 'C1': '061', 'C2': '062',
+          'C3': '063', 'Cac': '062', 'Car': '062',
+          'Cl': '170', 'F': '090', 'H': '010', 'HO': '010',
+          'I': '530', 'N1': '071', 'N2': '072',
+          'N3': '073', 'N3+': '073', 'Nam': '073',
+          'Nar': '072', 'Ng+': '072', 'Nox': '072',
+          'Npl': '072', 'Ntr': '071', 'O-': '085',
+          'O.co2': '082', 'O2': '082', 'O3': '083',
+          'P': '150', 'S2': '162', 'S3': '163',
+          'Sac': '162', 'Se': '340', 'So2': '162',
+          'Sox': '162'}
+'''Atom encoding
+Each atom encoding is represented by a string of numbers
+consisting of an element encoding and a hybrid encoding.
+Element encoding is done based off of the periodic table
+where the element that will be encoded is encoded to the
+corresponding element number on the periodic table.
+Type encoding is done based off of the type of atom it is.
+ _________________________
+| Element (XX) | Type (X) |
+Ex.
+ ____________       ________
+| Carbon | 3 | --> | 06 | 3 | --> 063
+'''
 
-    Element encoding is done based off of the periodic table
-    where the element that will be encoded is encoded to the
-    corresponding element number on the periodic table.
 
-    Type encoding is done based off of the type of atom it is.
-     _________________________
-    | Element (XX) | Type (X) |
+def molecular_info(mol, bond_len=200, angle_len=400, torsion_len=600, nb_len=1000, fill_val=-99999):
+    # Pad lists to ensure same length
+    # bond_len = 200
+    # angle_len = 400
+    # torsion_len = 600
+    # nb_len = 1000
+    # fill_val = -99999
 
-    Ex.
-     ____________       ________
-    | Carbon | 3 | --> | 06 | 3 | --> 063
+    # iterate through all bonds
+    bonds = []
+    for bond in ob.OBMolBondIter(mol.OBMol):
+        begin = atomType(mol, bond.GetBeginAtomIdx())
+        end = atomType(mol, bond.GetEndAtomIdx())
 
-    '''
-    element_encoding = {'B': '05', 'C': '06', 'F': '09',
-                        'I': '53', 'N': '07', 'O': '08',
-                        'P': '15', 'S': '16', 'H': '01',
-                        'Cl': '17', 'Br': '35'}
-    type_encoding = {'1': '1', '2': '2', 'a': '2', '3': '3', '+': '4'}
-    for atom_type in atom:
-        atom_type = atom[0:2]
-        if atom_type == (atom_type[0] + '1'):
-            atom_encoding = (str(element_encoding[atom[0]]) +
-                             str(type_encoding[atom[1]]))
-        elif atom_type == (atom_type[0] + '2'):
-            atom_encoding = (str(element_encoding[atom[0]]) +
-                             str(type_encoding[atom[1]]))
-        elif atom_type == (atom_type[0] + 'a'):
-            atom_encoding = (str(element_encoding[atom[0]]) +
-                             str(type_encoding[atom[1]]))
-        elif atom_type == (atom_type[0] + '3'):
-            atom_encoding = (str(element_encoding[atom[0]]) +
-                             str(type_encoding[atom[1]]))
-        elif atom_type == (atom_type[0] + '+'):
-            atom_encoding = (str(element_encoding[atom[0]]) +
-                             str(type_encoding[atom[1]]))
-        elif atom_type == (atom_type[0] + 'l'):
-            atom_encoding = str(element_encoding[atom[0:2]]) + '0'
-        elif atom_type == (atom_type[0] + 'r'):
-            atom_encoding = str(element_encoding[atom[0:2]]) + '0'
+        if (end < begin):
+            # swap them for lexographic order
+            begin, end = end, begin
+
+        bond_type = encode[begin] + encode[end]
+        bond_length = ("%8.4f" % (bond.GetLength()))
+        bonds.append(float(bond_type))
+        bonds.append(float(bond_length))
+
+    # check to see if length does not exceed index
+    if (len(bonds) > bond_len):
+        raise Exception('{} bonds. Change padding.'.format(len(bonds)))
+
+    bonds.extend([fill_val] * (bond_len - len(bonds)))
+
+    # iterate through all angles
+    angles = []
+    for angle in ob.OBMolAngleIter(mol.OBMol):
+        a = (angle[0] + 1)
+        b = mol.OBMol.GetAtom(angle[1] + 1)
+        c = (angle[2] + 1)
+
+        aType = atomType(mol, a)
+        cType = atomType(mol, c)
+        if (cType < aType):
+            # swap them for lexographic order
+            aType, cType = cType, aType
+
+        angle_type = (encode[aType] + encode[b.GetType()]
+                      + encode[cType])
+        angle_angle = ("%8.3f" % (b.GetAngle(a, c)))
+        angles.append(float(angle_type))
+        angles.append(float(angle_angle))
+
+    # check to see if length does not exceed index
+    if (len(angles) > angle_len):
+        raise Exception(
+            '{} angles. Change padding.'.format(len(angles)))
+
+    angles.extend([fill_val] * (angle_len - len(angles)))
+
+    # iterate through all torsions
+    torsions = []
+    for torsion in ob.OBMolTorsionIter(mol.OBMol):
+        a = (torsion[0] + 1)
+        b = (torsion[1] + 1)
+        c = (torsion[2] + 1)
+        d = (torsion[3] + 1)
+
+        aType = atomType(mol, a)
+        bType = atomType(mol, b)
+        cType = atomType(mol, c)
+        dType = atomType(mol, d)
+
+        # output in lexographic order
+        if (aType < dType):
+            torsion_type = (encode[aType] + encode[bType]
+                            + encode[cType] + encode[dType])
+            torsion_angle = ("%8.3f" %
+                             ((mol.OBMol.GetTorsion(a, b, c, d))))
+            torsions.append(float(torsion_type))
+            torsions.append(float(torsion_angle))
         else:
-            atom_encoding = str(element_encoding[atom[0]]) + '0'
-    return atom_encoding
+            torsion_type = (encode[dType] + encode[cType]
+                            + encode[bType] + encode[aType])
+            torsion_angle = ("%8.3f" %
+                             ((mol.OBMol.GetTorsion(a, b, c, d))))
+            torsions.append(float(torsion_type))
+            torsions.append(float(torsion_angle))
 
+    # check to see if length does not exceed index
+    if (len(torsions) > torsion_len):
+        raise Exception(
+            '{} torsions. Change padding.'.format(len(torsions)))
 
-# Pad lists to ensure same length
-bond_len = 200
-angle_len = 400
-torsion_len = 600
-nb_len = 7500
-pm7_len = 2
-fill_val = -99999
+    torsions.extend([fill_val] * (torsion_len - len(torsions)))
+
+    # iterate through all non-bonded
+    nb = []
+    for pair in ob.OBMolPairIter(mol.OBMol):
+        (first, second) = pair
+        begin_nb = atomType(mol, first)
+        end_nb = atomType(mol, second)
+        dist = mol.OBMol.GetAtom(first).GetDistance(second)
+        if (end_nb[0] < begin_nb[0]):
+            # swap them for lexographic order
+            begin_nb, end_nb = end_nb, begin_nb
+
+        nonbond_type = encode[begin_nb] + encode[end_nb]
+        nonbond_distance = ("%8.4f" % (dist))
+        nb.append(float(nonbond_type))
+        nb.append(float(nonbond_distance))
+
+    # check to see if length does not exceed index
+    # if (len(nb) > nb_len):
+    #     raise Exception(
+    #         '{} nonbonding. Change padding.'.format(len(nb)))
+    if len(nb) < nb_len:
+        nb.extend([fill_val] * (nb_len - len(nb)))
+    else:
+        nb = nb[:nb_len]
+
+    # Create molecular descriptor
+    n_rep = [*bonds, *angles, *torsions, *nb]
+
+    return n_rep
+
 
 row_list = []
 # Read through all the files in the folder of this directory
-for directory in glob.iglob(jobs_directory):
-    name = "/".join(directory.split('/')[6:10])  # name of the entry
+for directory in sorted(glob.iglob(jobs_directory + jobs_file)):
+    # name of the entry
+    name = "/".join(directory.split('/')[start_split:end_split])
+    short_name = name.split('.')[0]
+    # name = name + '.xyz'
 
-    for files in glob.iglob(directory + jobs_file):
-        conformer = files.split('/')[-1]  # conformer name/number
-        conf = conformer.split('.')[0]  # this splits off conformer name
-        name = str(name)
-        conf = str(' ' + conf)
+    try:
+        mol = next(pybel.readfile(jobs_file_format, directory))
 
-        # Use this for Python 2.7
-        # mol = pybel.readfile('format', argument).next()
-        # Use this for Python 3.6
-        mol = next(pybel.readfile(jobs_file_format, files))
+        # get the representation
+        n_rep = molecular_info(mol)
 
-        # iterate through all atoms
-        #  .. this is commented out because Bag Of Bonds doesn't use atomic charges
-        # for atom in mol.atoms:
-        #    print "Atom %d, %8.4f" % (atom.type, atom.partialcharge)
+        dict1 = {}
+        dict1.update({'Descriptor': n_rep})
+        dict1.update({'name': name})
+        dict1.update({'Molecule': short_name})
+        row_list.append(dict1)
 
-        # Running this try to ensure files and csv line up
-        # . If the molecule file does not exist in the csv
-        # . then it is skipped for reading in
-        try:
-            # pull energy out of csv
-            energy = (df_csv[(df_csv['Name'] == name)
-                             & (df_csv['Conformer'] == conf)].dftE.item())
+    except StopIteration:
+        pass
 
-            # pull pm7E info out of csv
-            pm7e = (df_csv[(df_csv['Name'] == name)
-                           & (df_csv['Conformer'] == conf)].pm7E.item())
-            pm7e = [pm7e]
-            pm7e.extend([fill_val])
+# dataframe of all of the bonds
+df = pd.DataFrame(row_list, columns=['name', 'Molecule', 'Descriptor'])
 
-            # iterate through all bonds
-            bonds = []
-            for bond in ob.OBMolBondIter(mol.OBMol):
-                begin = atomType(mol, bond.GetBeginAtomIdx())
-                end = atomType(mol, bond.GetEndAtomIdx())
+# merge the two dataframes
+combo = pd.merge(df, csv_all, on=['name'])
 
-                if (end < begin):
-                    # swap them for lexographic order
-                    begin, end = end, begin
-
-                bond_type = encoding(begin) + encoding(end)
-                bond_length = ("%8.4f" % (bond.GetLength()))
-                bonds.append(float(bond_type))
-                bonds.append(float(bond_length))
-
-            # check to see if length does not exceed index
-            if (len(bonds) > bond_len):
-                raise Exception('{} bonds. Change padding.'.format(len(bonds)))
-
-            bonds.extend([fill_val] * (bond_len - len(bonds)))
-
-            # iterate through all angles
-            angles = []
-            for angle in ob.OBMolAngleIter(mol.OBMol):
-                a = (angle[0] + 1)
-                b = mol.OBMol.GetAtom(angle[1] + 1)
-                c = (angle[2] + 1)
-
-                aType = atomType(mol, a)
-                cType = atomType(mol, c)
-                if (cType < aType):
-                    # swap them for lexographic order
-                    aType, cType = cType, aType
-
-                angle_type = (encoding(aType) + encoding(b.GetType())
-                              + encoding(cType))
-                angle_angle = ("%8.3f" % (b.GetAngle(a, c)))
-                angles.append(float(angle_type))
-                angles.append(float(angle_angle))
-
-            # check to see if length does not exceed index
-            if (len(angles) > angle_len):
-                raise Exception(
-                    '{} angles. Change padding.'.format(len(angles)))
-
-            angles.extend([fill_val] * (angle_len - len(angles)))
-
-            # iterate through all torsions
-            torsions = []
-            for torsion in ob.OBMolTorsionIter(mol.OBMol):
-                a = (torsion[0] + 1)
-                b = (torsion[1] + 1)
-                c = (torsion[2] + 1)
-                d = (torsion[3] + 1)
-
-                aType = atomType(mol, a)
-                bType = atomType(mol, b)
-                cType = atomType(mol, c)
-                dType = atomType(mol, d)
-
-                # output in lexographic order
-                if (aType < dType):
-                    torsion_type = (encoding(aType) + encoding(bType)
-                                    + encoding(cType) + encoding(dType))
-                    torsion_angle = ("%8.3f" %
-                                     ((mol.OBMol.GetTorsion(a, b, c, d))))
-                    torsions.append(float(torsion_type))
-                    torsions.append(float(torsion_angle))
-                else:
-                    torsion_type = (encoding(dType) + encoding(cType)
-                                    + encoding(bType) + encoding(aType))
-                    torsion_angle = ("%8.3f" %
-                                     ((mol.OBMol.GetTorsion(a, b, c, d))))
-                    torsions.append(float(torsion_type))
-                    torsions.append(float(torsion_angle))
-
-            # check to see if length does not exceed index
-            if (len(torsions) > torsion_len):
-                raise Exception(
-                    '{} torsions. Change padding.'.format(len(torsions)))
-
-            torsions.extend([fill_val] * (torsion_len - len(torsions)))
-
-            # iterate through all non-bonded
-            nb = []
-            for pair in ob.OBMolPairIter(mol.OBMol):
-                (first, second) = pair
-                begin_nb = atomType(mol, first)
-                end_nb = atomType(mol, second)
-                dist = mol.OBMol.GetAtom(first).GetDistance(second)
-                if (end_nb[0] < begin_nb[0]):
-                    # swap them for lexographic order
-                    begin_nb, end_nb = end_nb, begin_nb
-
-                nonbond_type = encoding(begin_nb) + encoding(end_nb)
-                nonbond_distance = ("%8.4f" % (dist))
-                nb.append(float(nonbond_type))
-                nb.append(float(nonbond_distance))
-
-            # check to see if length does not exceed index
-            if (len(nb) > nb_len):
-                raise Exception(
-                    '{} nonbonding. Change padding.'.format(len(nb)))
-
-            nb.extend([fill_val] * (nb_len - len(nb)))
-
-            dict1 = {}
-            dict1.update({'Bond': bonds})
-            dict1.update({'Angle': angles})
-            dict1.update({'Torsion': torsions})
-            dict1.update({'NB': nb})
-            dict1.update({'Name': name})
-            dict1.update({'Conformer': conf})
-            dict1.update({'Energy': energy})
-            dict1.update({'PM7E': pm7e})
-            row_list.append(dict1)
-
-        except ValueError:
-            pass
-
-
-# make a dataframe of all the molecules, their descriptors and energies
-df = pd.DataFrame(row_list, columns=['Name', 'Conformer',
-                                     'Energy',
-                                     'PM7E',
-                                     'Bond',
-                                     'Angle',
-                                     'Torsion',
-                                     'NB'])
-
-
-# make an array of all the parts of the molecular descriptors
-bonds = np.asarray(list(df['Bond']), dtype=np.float)
-angles = np.asarray(list(df['Angle']), dtype=np.float)
-torsions = np.asarray(list(df['Torsion']), dtype=np.float)
-nbs = np.asarray(list(df['NB']), dtype=np.float)
-pm7 = np.asarray(list(df['PM7E']), dtype=np.float)
-energy = np.asarray(list(df['Energy']), dtype=np.float)
-
-# make the molecular descriptor that will be used as the input
-molecule = np.concatenate((bonds, angles, torsions, nbs, pm7), axis=1)
-# print(molecule.shape) # if array size errors print to see shape
+# molecular descriptors for ML
+energy = np.asarray(list(combo[' dftE']), dtype=np.float)
+molecule = np.asarray(list(combo['Descriptor']), dtype=np.float)
 
 # test, train, split for data
 X_train, X_test, y_train, y_test = train_test_split(
@@ -284,14 +227,12 @@ X_train, X_test, y_train, y_test = train_test_split(
 # print(y_train.shape) # if array size errors print to see shape
 
 # specify regressor and run
-clf = BayesianRidge()
-clf.fit(X_train, y_train)
-accuracy = clf.score(X_test, y_test)
+regr = RandomForestRegressor(n_jobs=-1)
+regr.fit(X_train, y_train)
+accuracy = regr.score(X_test, y_test)
 r2 = ("%8.3f" % accuracy)
 print('r^2 =', accuracy)
-
-predicted = clf.predict(X_test)
-
+predicted = regr.predict(X_test)
 mae = mean_absolute_error(y_test, predicted)
 print('Mean Absolute Error =', mae)
 
